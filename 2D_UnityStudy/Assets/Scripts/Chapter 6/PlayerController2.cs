4fb995c2dca7e557422b4dcca5529af9bc3690ab
은총 <shhshsjsjd4@gmail.com>
2023-02-18 22:55:02 +0900

    제 6장 학습
    2023-02-18 유니티 교과서 298페이지까지 학습

---
+++ 2D_UnityStudy/Assets/Scripts/Chapter 6/PlayerController2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float mawWalkSpeed = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 점프한다
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 좌우 이동
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // 플레이어의 속도
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // 스피드 제한
+        if(speedx < this.mawWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2.0f;
+    }
+}
