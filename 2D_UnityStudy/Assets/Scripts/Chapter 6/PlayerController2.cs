113c017e4ecb66b942789e158bb88a1d0c993327
은총 <shhshsjsjd4@gmail.com>
2023-02-20 18:42:19 +0900

    제 6장 버그 수정
    2023-02-20 유니티 교과서 328페이지까지 학습

--- 2D_UnityStudy/Assets/Scripts/Chapter 6/PlayerController2.cs
+++ 2D_UnityStudy/Assets/Scripts/Chapter 6/PlayerController2.cs
@@ -10,6 +10,7 @@ public class PlayerController2 : MonoBehaviour
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float mawWalkSpeed = 2.0f;
+    float threshold = 0.2f;
     
     // Start is called before the first frame update
     void Start()
@@ -22,15 +23,16 @@ public class PlayerController2 : MonoBehaviour
     void Update()
     {
         // 점프한다
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         // 좌우 이동
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < this.threshold) key = -1;
 
         // 플레이어의 속도
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
@@ -41,13 +43,30 @@ public class PlayerController2 : MonoBehaviour
             this.rigid2D.AddForce(transform.right * key * this.walkForce);
         }
 
+        // 움직이는 방향에 따라 반전한다
         if(key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
 
-        // 플레이어 속도에 맞춰 애니메이션 속도를 바꾼다.
+        // 플레이어의 속도에 맞춰서 애니메이션 속도를 바꾼다
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+        // 플레이어 속도에 맞춰 애니메이션 속도를 바꾼다
         this.animator.speed = speedx / 2.0f;
+
+        // 플레이어가 화면 밖으로 나갔다면 처음부터
+        if(transform.position.y < -10)
+        {
+            SceneManager.LoadScene("Chapter 6(1)");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
