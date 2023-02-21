43729c52d1351e2c3fde90581cf7a59ec8535c34
은총 <shhshsjsjd4@gmail.com>
2023-02-21 22:13:30 +0900

    제 7장 학습 완
    2023-02-20 유니티 교과서 382페이지까지 학습

--- 3D_UnityStudy/Assets/Scripts/Chapter 7/BamsongiGenerator.cs
+++ 3D_UnityStudy/Assets/Scripts/Chapter 7/BamsongiGenerator.cs
@@ -12,7 +12,10 @@ public class BamsongiGenerator : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             GameObject bamsongi = Instantiate(bamsongiPrefab) as GameObject;
-            bamsongi.GetComponent<BamsongiController>().Shoot(new Vector3(0, 200, 2000));
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bamsongi.GetComponent<BamsongiController>().Shoot(worldDir.normalized * 2000);
         }
     }
 }
